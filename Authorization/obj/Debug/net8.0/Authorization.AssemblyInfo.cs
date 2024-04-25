3bd23dc3a1fed77ccbaf41f79027eb1f49c1875e
saidr <saidradwanmansour@hotmail.com>
2024-04-25 17:29:03 +0200

    new changes

--- Authorization/obj/Debug/net8.0/Authorization.AssemblyInfo.cs
+++ Authorization/obj/Debug/net8.0/Authorization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authorization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae25dcab47a6f24ea440d16e09d474d5153854b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authorization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authorization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
